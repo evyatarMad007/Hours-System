0a4eacd233b9cd35510b3cde544be8f95daa78ba
Evyatar-M <64112773+evyatarMad007@users.noreply.github.com>
2021-06-02 05:48:15 +0300

    updated & added.
    Add new project area:
    ○ Built in UI (not yet configured functionally)
    
    ------------------------------------------
    
    אזור הוספת פרויקט חדש:
    ○  נבנה ב- UI (טרם הוגדר פונקציונלית)

--- src/components/util/add-project/add-project.jsx
+++ src/components/util/add-project/add-project.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { MdCreateNewFolder } from 'react-icons/md';
 
 
-function AddProject() {
-  
+class AddProject extends Component {
+  state = { 
+    newProjectMode: false
+  }
+
+  createNewProject = () => {
+    let { newProjectMode } = this.state;
+    newProjectMode ? newProjectMode = false : newProjectMode = true;
+    this.setState({ newProjectMode });
+  }
+
+  render() { 
+    const { newProjectMode } = this.state;
+    console.log(newProjectMode);
     return (
       <div className="add-row">
           <div className="add-project-name">
             
-            <button className="btn btn-success">צור פרוייקט  <span><MdCreateNewFolder/></span></button>
-            <div className="inputs">
-               <input  className="form-control" type="text"  placeholder="שם הפרוייקט"/>
+            <button className="btn btn-success" onClick={this.createNewProject}>צור פרוייקט חדש <span><MdCreateNewFolder/></span></button>
+            <div className={newProjectMode ? "inputs inputs-on" : "inputs"}>
+               <div className="project-info">
+                 <p className="display-4">פרטי הפרוייקט</p>
+                    <div className="project-general">
+                        <input  className="form-control" type="text"  placeholder="שם הפרוייקט"/>
+                        <input  className="form-control" type="text"  placeholder="תעריף שעתי"/>
+                    </div>
+               </div>
+               <div className="consumer-info">
+                    <div className="consumer-general">
+                    <p className="display-4">פרטי הלקוח</p>
+                        <input  className="form-control" type="text"  placeholder="שם פרטי"/>
+                        <input  className="form-control" type="text"  placeholder="שם משפחה"/>
+                        <input  className="form-control" type="text"  placeholder="כתובת מגורים"/>
+                    </div>
+                    <div className="consumer-contact">
+                        <input  className="form-control" type="text"  placeholder="מספר טלפון"/>
+                        <input  className="form-control" type="text"  placeholder="כתובת מייל"/>
+                    </div>
+               </div>
             </div>
               
               
@@ -20,5 +50,6 @@ function AddProject() {
       </div>
     );
   }
-  
-  export default AddProject;
+}
+ 
+export default AddProject;
