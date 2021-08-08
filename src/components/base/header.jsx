0333c2aaedf85847e55ea5c9dd0d2c9d7aee7f33
Evyatar-M <64112773+evyatarMad007@users.noreply.github.com>
2021-08-08 03:31:14 +0300

    ui header and navbar hass been updated

--- src/components/base/header.jsx
+++ src/components/base/header.jsx
@@ -34,14 +34,14 @@ class Header extends Component {
                     <div className="header__content__right-side">
                     { userService.getCurrentUser() 
                     ? <NavLink className="navLink profile-navLink" to="/profile">
-                    <span className="username">{! this.props.userData ? '' : this.props.userData.firstName}</span>
+                    <span className="username">{! this.props.userData ? '' : `${this.props.userData.firstName}  ${this.props.userData.lastName}`}</span>
                     </NavLink>
                     : ''} 
                         <div className="profile-img">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnS1o3mO3S_Nkfw1WAGaRJ6KaOGgODpfoOsA&usqp=CAU" alt="" />
                         </div>
                         { userService.getCurrentUser() 
-                        ? <button onClick={this.logout}>Logout </button> 
+                        ? <button className="logout" onClick={this.logout}>Logout </button> 
                         : <React.Fragment>
                             <NavLink className="navLink signin-navLink" to="/signin"><div className="signin-btn">Sign in <RiKeyFill/></div></NavLink>
                             <NavLink className="navLink signup-navLink" to="/signup"><div className="signup-btn">Sign up <FaWpforms/></div></NavLink>
