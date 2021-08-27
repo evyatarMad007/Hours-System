043fd8a5b455af75ff0748bbef4165e4abdacb05
Evyatar-M <64112773+evyatarMad007@users.noreply.github.com>
2021-08-27 04:32:59 +0300

    profile page has been create by UI and Design

--- src/components/main-area/profile-page.jsx
+++ src/components/main-area/profile-page.jsx
@@ -5,6 +5,14 @@ import axios from 'axios';
 import { Redirect } from "react-router-dom";
 import PageHeader from '../common/page-header';
 import {headersAuth} from '../../utils/constData';
+import { FaUserAlt } from 'react-icons/fa';
+import { GiPresent } from 'react-icons/gi';
+import { CgGenderMale } from 'react-icons/cg';
+import { RiUser6Fill, RiFacebookFill,RiLinkedinFill,RiGithubFill, RiToolsFill, RiMedalFill,RiUserSmileFill } from 'react-icons/ri';
+import { MdLocationCity, MdPhone, MdEmail } from 'react-icons/md';
+import { HiOutlineMail } from 'react-icons/hi';
+import { GoNote } from 'react-icons/go';
+import { BsFillStarFill } from 'react-icons/bs';
 
 class Profile extends Component {
     state = { 
@@ -32,10 +40,7 @@ class Profile extends Component {
 
     render() { 
         if( ! userService.getCurrentUser() ) return <Redirect to="/"/>
-
         const { error, isLoaded, userData } = this.state;
-
-
         if (error) {
             return <div>Error: {error.message}</div>;
         }
@@ -43,38 +48,52 @@ class Profile extends Component {
             return <div>Loading...</div>;
         } 
         else {
-
             return ( 
                 <div className="profile">
 
                     <div className="profile__page-header" style={{ backgroundImage: 'url(https://www.ceres.org/sites/default/files/2019-10/nyc-skyline-banner.jpg)' }}>
-
                         <div className="img-profile-box" style={{ backgroundImage: 'url(https://evyatarmadari.co.il/static/media/profile.427dc728.jpg)' }}>
                         <button type="button" className="upload-img-btn">+
                         
                         </button>
                         </div>
+                        <button type="button" className="upload-img-btn">+
+                        </button>
                     </div>
-
                     <div className="profile__page-content">
                         <div className="profile-title">
                             <p className="full-name">Evyatar Madari</p>
                             <p className="proffession-name">Developer</p>
                         </div>
-
-
                         <div className="profile-details">
-                            {
+                            <div className="basic-info">
+                                <ul>
+                                    <li><div className="icon"><RiUser6Fill/></div><div className="text">{userData.firstName ? userData.firstName : <i>empty</i>}</div></li>
+                                    <li><div className="icon"><RiUser6Fill/></div><div className="text">{userData.lastName ? userData.lastName  : <i>empty</i>}</div></li>
+                                    <li><div className="icon"><GiPresent/></div><div className="text">{userData.age ? userData.age : <i>empty</i>}</div></li>
+                                    <li><div className="icon"><CgGenderMale/></div><div className="text">{userData.gender ? userData.gender : <i>empty</i>}</div></li>
+                                    <li><div className="icon"><MdLocationCity/></div><div className="text">{userData.cityAdress ? userData.cityAdress : <i>empty</i>}</div></li>
+                                </ul>
+                            </div>
+                            <div className="business-info">
                             <ul>
-                                <li>First Name: {userData.firstName}</li>
-                                <li>Last Name: {userData.lastName}</li>
-                                <li>Age: {userData.age}</li>
-                                <li>Gender: {userData.gender}</li>
-                                <li>City Adress: {userData.cityAdress}</li>
-                                <li>Email: {userData.email}</li>
-                                <li>Phone Number: {userData.phoneNumber}</li>
-                            </ul>
-                            }
+                            <li><div className="icon"><GoNote/></div><div className="text">{userData.someData ? userData.someData :  <i>empty</i>}</div></li>
+                                    <li><div className="icon"><RiToolsFill/></div><div className="text">{userData.someData ? userData.someData :  <i>empty</i>}</div></li>
+                                    <li><div className="icon"><MdPhone/></div><div className="text">{userData.phoneNumber ? userData.phoneNumber :  <i>empty</i>}</div></li>
+                                    <li><div className="icon"><HiOutlineMail/></div><div className="text">{userData.email ? userData.email :  <i>empty</i>}</div></li>
+                                    <li><div className="icon"><RiFacebookFill/></div><div className="text">{userData.someData ? userData.someData :  <i>empty</i>}</div></li>
+                                    <li><div className="icon"><RiLinkedinFill/></div><div className="text">{userData.someData ? userData.someData :  <i>empty</i>}</div></li>
+                                    <li><div className="icon"><RiGithubFill/></div><div className="text">{userData.someData ? userData.someData :  <i>empty</i>}</div></li>
+                                </ul>
+                            </div>
+                            <div className="explanation-info">
+                            <ul>
+                                <li className="users-type-title">Types Of Accounts</li>
+                                <li><div className="icon basic"><RiUserSmileFill/></div><div className="text"><i>Basic - under 20 projects</i></div></li>
+                                    <li><div className="icon star"><BsFillStarFill/></div><div className="text"><i>Advance - Over 10 projects</i></div></li>
+                                    <li><div className="icon premium"><RiMedalFill/></div><div className="text"><i>Premium - full Tools</i></div></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
